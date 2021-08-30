3ef8d3f7bda602c40bfbd5f45ec3edd0372cf556
Vivek131299 <vivek131299@gmail.com>
2021-08-30 15:58:23 +0530

    Adding Remaining CRUD Methods and Testing REST API with JPA DAO Implementation

--- src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
+++ src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
@@ -35,16 +35,37 @@ public class EmployeeDAOJpaImpl implements EmployeeDAO {
 
     @Override
     public Employee findById(int theId) {
-        return null;
+
+        // get the employee
+        Employee theEmployee = entityManager.find(Employee.class, theId);
+        // find() is similar to Hibernate's get() or load().
+
+        // return the employee
+        return theEmployee;
     }
 
     @Override
     public void save(Employee theEmployee) {
+
+        Employee dbEmployee = entityManager.merge(theEmployee);
+        // merge() is similar to Hibernate's saveOrUpdate().
+        // So, id id=0, then it will insert/save OR else if id is given then it will update.
+        // AND JPA's persist() is similar to Hibernate's save().
+
+        // update with id from db... so we can get generated id for save/insert.
+        theEmployee.setId(dbEmployee.getId());
 
     }
 
     @Override
     public void deleteById(int theId) {
 
+        // delete object with primary key
+        Query theQuery = entityManager.createQuery("delete from Employee where id=:employeeId");
+        theQuery.setParameter("employeeId", theId);
+
+        theQuery.executeUpdate();
+
+        // We can use JPA's remove() which is similar to Hibernate's delete().
     }
 }
