a06a9e0099eabf9b8b64fd17a598fad898beae4c
Vivek131299 <vivek131299@gmail.com>
2021-08-30 14:59:00 +0530

    REST Controller Methods to Find, Add, Update and Delete Employee

--- src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -4,9 +4,7 @@ import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import com.luv2code.springboot.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,4 +26,54 @@ public class EmployeeRestController {
 
         return employeeService.findAll();
     }
+
+    // add mapping for GET /employees/{employeeID}
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if (theEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        return theEmployee;
+    }
+
+    // add mapping for POST /employees - adding a new employee
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee) {
+    // @RequestBody is used to handle and binding the incoming JSON data from request body to our Employee object.
+
+        // Also, just in case, if user pass an id in JSON, then we set it to 0. because if id=0 then our saveOrUpdate() will insert new employee.
+        theEmployee.setId(0);
+
+        employeeService.save(theEmployee);
+
+        return theEmployee;
+    } // We tested this method in Postman software.
+
+    // add mapping for PUT /employees - Updating existing employee
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee) {
+        employeeService.save(theEmployee);
+
+        return theEmployee;
+    } // We tested this method in Postman software.
+
+    // add mapping for DELETE /employees/{employeeId} - Deleting the employee
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId) {
+
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        // throw exception if null
+        if (tempEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        employeeService.deleteById(employeeId);
+
+        return "Deleted employee id - " + employeeId;
+    } // We tested this method in Postman software.
 }
