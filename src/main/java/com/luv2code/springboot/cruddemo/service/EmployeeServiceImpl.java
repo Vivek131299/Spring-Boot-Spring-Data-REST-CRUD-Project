812ec97ecb8f959371eb6c25bce39e5dbadc5ddb
Vivek131299 <vivek131299@gmail.com>
2021-08-30 15:42:05 +0530

    Creating JPA DAO Implementation for REST API

--- src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package com.luv2code.springboot.cruddemo.service;
 import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,7 +16,13 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     // Constructor Injection of our DAO
     @Autowired
-    public EmployeeServiceImpl(EmployeeDAO theEmployeeDAO) {
+    public EmployeeServiceImpl(@Qualifier("employeeDAOJpaImpl") EmployeeDAO theEmployeeDAO) {
+    // Here, @Qualifier ANNOTATION, since we are using JPA API instead of Hibernate in this project,
+    // and we hav both DAO implementations (for JPA and Hibernate as well), So the parameter above (EmployeeDAO)
+    // doesn't know which API DAO Impl to use.
+    // So, we are giving @Qualifier("employeeDAOJpaImpl") to tell that we want to use this Bean id (employeeDAOJpaImpl).
+    // So now, it will use JPA Impl (employeeDAOJpaImpl is a Bean id for EmployeeDAOJpaImpl) without any error.
+
         employeeDAO = theEmployeeDAO;
     }
 
