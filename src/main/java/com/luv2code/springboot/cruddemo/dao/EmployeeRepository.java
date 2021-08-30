29cca7deb5cd52208360a1d4b25e7f3e94fb620c
Vivek131299 <vivek131299@gmail.com>
2021-08-30 17:36:30 +0530

    Spring Data REST Configuration, Pagination and Sorting

--- src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,10 +2,13 @@ package com.luv2code.springboot.cruddemo.dao;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 // This is our Spring Data JPA Repository instead of Hibernate or JPA API.
 // This provides all basic CRUD operations for free.
 // So we don't need to write any methods for CRUD operations.
+
+/*@RepositoryRestResource(path = "members")*/ // So now, we need to go to /magic-api/members instead of /magic-api/employees.
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 // Above, 'Employee' is a Entity type and 'Integer' is a primary key type.
 
